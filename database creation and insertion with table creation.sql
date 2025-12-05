9a6fb507a60a5b7c8936ba92846ef8d64bbaf2bb
SaisaKoirala <saisakoirala01@gmail.com>
2025-12-05 16:24:12 +0545

    initial commit

---
+++ database creation and insertion with table creation.sql
@@ -0,0 +1,14 @@
+--CREATE DATABASE DB;
+USE DB;
+
+CREATE TABLE students(
+id int primary key,
+name varchar(30)
+);
+
+INSERT INTO students
+VALUES(1 , 'sasa');
+INSERT INTO students
+VALUES(2, 'sama');
+
+SELECT * FROM students;
