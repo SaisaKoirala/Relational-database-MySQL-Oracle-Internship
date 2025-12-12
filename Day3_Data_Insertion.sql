cfdfa0ee1d6615a31c8a5f6ce0eae5324392b2df
SaisaKoirala <saisakoirala01@gmail.com>
2025-12-12 07:31:30 +0545

    Create Day3_Data_Insertion.sql

---
+++ Day3_Data_Insertion.sql
@@ -0,0 +1,7 @@
+
+--DAY 3: Data Insertion
+
+INSERT INTO students VALUES (seq_students.NEXTVAL, 'Asha Rai', 20, 'BSc');
+INSERT INTO students VALUES (seq_students.NEXTVAL, 'Bibek Thapa', 22, 'BBA');
+INSERT INTO students VALUES (seq_students.NEXTVAL, 'Sasa San', 22, 'CSIT');
+INSERT INTO students VALUES (seq_students.NEXTVAL, 'Sama Bro', 19, 'CSIT');
